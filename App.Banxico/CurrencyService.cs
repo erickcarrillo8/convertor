8d91217546eea1f07aeac2a73ad1fed671c029f8
erickcarrillo8 <erickcarrillo8@gmail.com>
2016-08-29 12:05:48 -0700

    Banxico layer added

---
+++ App.Banxico/CurrencyService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Banxico
+{
+    public class CurrencyService
+    {
+        #region Properties
+        /// <summary>
+        /// Date of currency value, individually
+        /// </summary>
+        public string Date { get; set; }
+        /// <summary>
+        /// Currency value in pesos
+        /// </summary>
+        public float Value { get; set; }
+        #endregion
+    }
+}
